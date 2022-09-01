42f2cd4991b31e8b1f844ef483a53b32522d6eac
RenanFerreira02 <renankferreira01@gmail.com>
2022-08-31 22:34:48 -0300

    Aula 06 - Introdução a Loops, início com loop For

---
+++ 2° Módulo/PW-II/Aula06/src/loop_para/loop_alternativo.java
@@ -0,0 +1,18 @@
+package loop_para;
+
+
+public class loop_alternativo {
+    public static void main(String[] args) {
+        
+        
+        
+        
+        for(int i = 0, u = 1; i <= 100 && u <= 100; i = i + 2, u = u + 2)  {
+           
+            System.out.println("i = " + i + " :: " + "u = " + u);
+            
+            
+        }
+        
+    }
+}
---
+++ 2° Módulo/PW-II/Aula06/src/loop_para/loop_para.java
@@ -0,0 +1,40 @@
+package loop_para;
+
+public class loop_para {
+
+    public static void main(String[] args) {
+
+        int i, soma_par, soma_impar;
+
+        soma_par = 0;
+        soma_impar = 0;
+
+        for (i = 0; i <= 100; i++) {
+
+            if (i % 2 == 0) {
+                System.out.println(i + " Par");
+                soma_par = soma_par = soma_par + i;
+
+            } else {
+                System.out.println(i + " Ímpar");
+                soma_impar = soma_impar = soma_impar + i;
+            }
+
+        }
+
+        System.out.println("");
+        System.out.println("Total par: " + soma_par);
+        System.out.println("");
+
+        System.out.println("");
+        System.out.println("Total ímpar: " + soma_impar);
+        System.out.println("");
+
+        // Comparar os valores totais e imprimir qual que é o maior
+        if (soma_par > soma_impar) {
+            System.out.println("O Total par é maior que o total de ímpar");
+        } else {
+            System.out.println("O Total ímpar é maior que o total de par");
+        }
+    }
+}
