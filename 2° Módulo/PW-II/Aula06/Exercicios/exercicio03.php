561a2137450f4af0f8d6a38bd19e822307eaef4b
RenanFerreira02 <renankferreira01@gmail.com>
2022-09-08 18:25:35 -0300

    Exercício 03 PHP

--- 2° Módulo/PW-II/Aula06/Exercicios/exercicio03.php
+++ 2° Módulo/PW-II/Aula06/Exercicios/exercicio03.php
@@ -5,16 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Exercício 3</title>
 
-    <style>
-        table,
-        th,
-        td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-    </style>
+
 </head>
 
 <body>
@@ -23,62 +17,47 @@
             <tr>
                 <th colspan="2">Tabela</th>
             </tr>
+
+            <tr>
+                <th>Produto</th>
+                <th>Valor</th>
+            </tr>
         </thead>
 
-        <tr>
-            <th>Produto</th>
-            <th>Valor</th>
-        </tr>
-        
-        <tr>
-            <td>1</td>
-            <td>1</td>
-        </tr>
+        <tbody>
 
-        <tr>
-            <td>BMW M4</td>
-            <td>R$ 50.000,00</td>
-        </tr>
+            <?php
 
-        <tr>
-            <td>Audi A7</td>
-            <td>R$ 75.000,00</td>
-        </tr>
+            // $produto;
 
-        <tr>
-            <td>4</td>
-            <td>4</td>
-        </tr>
+            // $valor;
 
-        <tr>
-            <td>5</td>
-            <td>5</td>
-        </tr>
+            // for ($produto = 1, $valor = 1; $produto <= 10 && $valor <= 10; $produto++, $valor++) {
 
-        <tr>
-            <td>6</td>
-            <td>6</td>
-        </tr>
+            //     echo "<tr class='tr_ex03'>
+            //             <td>$produto</td>
+            //             <td>$valor</td>
+            //           </tr>";
+            // }
 
-        <tr>
-            <td>7</td>
-            <td>7</td>
-        </tr>
+            $produtos = array('Abacaxi', 'Laranja', 'Manga', 'Maça', 'Pera', 'Tomate', 'Jabuticaba', 'Banana', 'Morango', 'Melancia');
 
-        <tr>
-            <td>8</td>
-            <td>8</td>
-        </tr>
+            $valores = array(10.0, 3.50, 7.0, 9.0, 5.0, 3.50, 15.0, 12.0, 10.0, 30.0);
 
-        <tr>
-            <td>9</td>
-            <td>9</td>
-        </tr>
 
-        <tr>
-            <td>10</td>
-            <td>10</td>
-        </tr>
+            //count = conta a quantidade de indice do array
+
+            for ($i = 0; $i < count($produtos); $i++) {
+
+                echo "<tr class='tr_ex03'>
+                        <td>$produtos[$i]</td>
+                        <td>$valores[$i]</td>
+                 </tr>";
+            }
+            ?>
+
+        </tbody>
+
     </table>
 </body>
 
