601e15ccf6d386e85cf0ad7d02288c3dfeebbfb0
RenanFerreira02 <renankferreira01@gmail.com>
2023-02-16 22:22:59 -0300

    Aula 02 PHP - Introdução a MVC, criação e rota das páginas

---
+++ Módulo 3/PW - III/Aula 2/Laravel/resources/views/framework.blade.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="{{url('css/style.css')}}" />
+		<title>Definição de Framework</title>
+	</head>
+	<body>
+		<section class="introducao">
+			<h1>Framework</h1>
+
+			<p>
+				Um framework é uma estrutura que serve de base para a construção de aplicações web de
+				finalidade específica cujo desenvolvimento pode ser muito custoso e/ou problemático. Com um
+				framework é possível construir sites, aplicativos e softwares a partir de um esqueleto
+				pré-definido, alterando apenas demais particularidades. Os frameworks podem ser front-end ou
+				back-end.
+			</p>
+
+			<img src="{{url('images/frameworks.png')}}" alt="" width="500" />
+		</section>
+
+		<section class="funcao-framework">
+			<h3>Para que serve?</h3>
+
+			<p>
+				Um framework serve para auxiliar o programador ou programadora na hora de construir uma
+				aplicação web que já foi amplamente utilizada por outros especialistas.
+			</p>
+
+			<p>
+				Por exemplo, na hora de desenvolver um site de vendas, ou um e-commerce, o que seria mais
+				vantajoso:
+			</p>
+
+			<ul>
+				<li>construir todo o site do zero, inclusive toda sua estrutura logística, ou</li>
+
+				<li>
+					fazer uso de um framework para lojas virtuais que já vem com todo suporte de
+					funcionalidades e comandos previamente desenvolvido, faltando as especificações técnicas
+					relativas à loja que o utilizará?
+				</li>
+			</ul>
+
+			<p>
+				A opção 2 é a que traz mais benefícios, não é mesmo? Pois bem, é para isso que serve um
+				framework: para que uma aplicação possa ser desenvolvida de maneira mais ágil, rápida e sem
+				obstáculos relacionados a problemas que já foram resolvidos por outros profissionais.
+			</p>
+		</section>
+
+		<section class="vantagens-desvantagens">
+			<h3>Vantagens</h3>
+
+			<p>
+				As vantagens de utilizar um framework são inúmeras, desde uma maior otimização de tempo até
+				o impacto positivo em todos os setores de uma companhia, uma vez que o tempo dos
+				profissionais gastos em códigos reescritos pode ser voltado à inovação, aqui vai uma lista
+				de algumas vantagens.
+			</p>
+
+			<ul>
+				<li>
+					Aumento da produtividade: Um tempo que deixa de ser gasto desnecessariamente dá a um
+					profissional a chance de aplicá-lo a algo que pode estar necessitando de uma maior
+					atenção. Ao construir aplicações com maior rapidez e fluidez, a produtividade de um
+					programador ou programadora também se beneficia, o tornando apto a fazer mais em menor
+					tempo menor sem abrir mão da qualidade de seu trabalho.
+				</li>
+
+				<li>
+					Fácil manutenção: Um framework é um código padronizado, portanto há uma manutenção
+					padronizada também. Na hora de fazer uma melhoria ou consertar algum ponto de atenção, não
+					será necessário quebrar a cabeça em busca de uma solução desconhecida: basta acessar a
+					documentação do framework e encontrar a resposta.
+				</li>
+
+				<li>
+					Maior segurança: Com uma menor chance de erros de execução e uma manutenção simples, os
+					frameworks são tecnologias que garantem mais segurança ao desenvolvimento de softwares,
+					tanto para os profissionais de TI quanto para seus clientes.
+				</li>
+
+				<li>
+					Reduz custos: Se tempo é dinheiro, a economia de tempo também pode ser encarada como uma
+					economia financeira. Ao obter um melhor aproveitamento do time de profissionais e de sua
+					produtividade, os custos de manutenção de um projeto também caem.
+				</li>
+			</ul>
+
+			<p>
+				Sob o ponto de vista comercial, esta é uma grande vantagem competitiva: enquanto seu
+				concorrente é capaz de entregar 2 projetos ao ano, você será capaz de entregar 4.
+			</p>
+
+			<h3>Desvantagens</h3>
+
+			<p>
+				Como em tecnologia da informação nem tudo são flores, os frameworks também possuem algumas
+				desvantagens técnicas, que são:
+			</p>
+
+			<ul>
+				<li>
+					Causa dependência: Os frameworks devem ser utilizados por profissionais experientes que já
+					possuam um razoável grau de familiaridade tanto com a aplicação desenvolvida quanto com a
+					linguagem de programação que este framework usa. Isso porque, na posição de estudante,
+					ingressar no mercado de trabalho muito habituado ao uso de frameworks pode te impedir de
+					adquirir importantes capacidades, principalmente àquelas relacionadas à resolução de
+					problemas. Também é importante que jovens profissionais conheçam o caminho das pedras do
+					desenvolvimento web para que, no futuro, possam ter uma visão mais ampla de seus projetos.
+				</li>
+
+				<li>
+					Dificuldade em configurar: Por utilizar códigos fixos e imutáveis, muitas vezes os
+					frameworks podem apresentar algumas dificuldades de configuração. Isso acontece,
+					principalmente, quando os profissionais envolvidos em seu uso não possuem familiaridade
+					com suas tecnologias. Caso este obstáculo se apresente, recomendamos a leitura atenta da
+					documentação deste framework e a interação com outros profissionais da comunidade.
+				</li>
+
+				<li>
+					Mais códigos que o necessário: Pode acontecer, também, de utilizarmos um framework inteiro
+					para desenvolver uma aplicação que precisa somente de uma parte das funções oferecidas por
+					ele. Neste caso, os códigos que "sobrarem" poderão acarretar um peso desnecessário ao
+					projeto. Se a aplicação desenvolvida for um site, por exemplo, é preciso tomar cuidado
+					para que este código adicional não represente maior tempo de carregamento das páginas.
+				</li>
+			</ul>
+		</section>
+	</body>
+</html>
---
+++ Módulo 3/PW - III/Aula 2/Laravel/resources/views/tutorial.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="{{url('css/style.css')}}" />
+		<title>Criando um projeto com Laravel</title>
+	</head>
+
+	<body>
+		<h1>Tutorial</h1>
+
+		<ol>
+			<li>Abra o CMD</li>
+
+			<li>Utilize o comando <code>cd/</code> para retornar a pasta raiz do seu computador</li>
+
+			<li>
+				Crie um diretorio usando <code>mkdir "Nome da pasta"</code> substitua "Nome da pasta" pelo
+				nome desejado
+			</li>
+
+			<li>Abra o diretório utilizando <code>cd "Nome da pasta"</code></li>
+
+			<li>
+				Após acessar a pasta digite
+				<code>composer create-project laravel/laravel "Nome de uma pasta"</code>
+			</li>
+
+			<li>Conclua a instalação do Framework</li>
+
+			<li>
+				Utilize o comando <code>cd "Nome de uma pasta"</code> para acessar a pasta do Framework
+			</li>
+
+			<li>
+				Ao acessar a pasta, coloque <code>code .</code> no CMD e aperte enter para abrir o Visual
+				Studio Code
+			</li>
+
+			<li>
+				Ao abrir o VS Code volte para o cmd e digite <code>php artisan serve</code> para iniciar o
+				servidor do Framework
+			</li>
+
+			<li>
+				Copie o endereço que aparece no CMD ou digite localhost:8000 no seu navegador para acessar a
+				página
+			</li>
+		</ol>
+	</body>
+</html>
