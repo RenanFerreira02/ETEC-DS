2818df7e0eb0cecaa2482197ba25ef5fb2a81b19
RenanFerreira02 <renankferreira01@gmail.com>
2023-03-20 22:27:33 -0300

    Aula 05 - Criando um componente com funções de contador Incremento e Decremento

---
+++ Módulo 3/PAM -II/React Native/Aula 05/Contador/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Contador from './components/Contador';
+
+export default function App() {
+	return (
+		<View>
+			<Contador />
+		</View>
+	);
+}
